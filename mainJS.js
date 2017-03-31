ecbf1e554387f6108b9a0fe38fbb28c08873634f
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2017-03-31 02:52:40 -0400

    Making small adjustments to help

--- mainJS.js
+++ mainJS.js
@@ -1,7 +1,7 @@
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementsById('map'), {
-          center: {lat: -40.8054491, lng: -73.9654415},
-          zoom: 8
-        });
-      }
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: {lat: -40.8054491, lng: -73.9654415},
+	  zoom: 8
+	});
+}
